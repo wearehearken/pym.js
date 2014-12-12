2185228503b89c470202f2fc764feca1be3b4aa1
Christopher Groskopf <staringmonkey@gmail.com>
2014-12-12 10:48:08 -0600

    Add browserify module definition. Closes #60.

--- src/pym.js
+++ src/pym.js
@@ -6,6 +6,8 @@
 (function(factory) {
     if (typeof define === 'function' && define.amd) {
         define(factory);
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = factory();
     } else {
         window.pym = factory.call(this);
     }
