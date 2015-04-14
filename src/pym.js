d23395b9f2cf755929294c50b8cda28707a07d95
Christopher Groskopf <staringmonkey@gmail.com>
2015-04-14 10:23:44 -0500

    Experimental scrollto support.

--- src/pym.js
+++ src/pym.js
@@ -139,7 +139,7 @@
             xdomain: '*'
         };
 
-        this.messageRegex = _makeMessageRegex(this.id); 
+        this.messageRegex = _makeMessageRegex(this.id);
         this.messageHandlers = {};
 
         // ensure a config object
@@ -156,7 +156,7 @@
             var width = this.el.offsetWidth.toString();
 
             // Create an iframe element attached to the document.
-            this.iframe = document.createElement("iframe");
+            this.iframe = document.createElement('iframe');
 
             // Save fragment id
             var hash = '';
@@ -174,7 +174,7 @@
             } else {
                 this.url += '&';
             }
-            
+
             // Append the initial width as a querystring parameter, and the fragment id
             this.iframe.src = this.url + 'initialWidth=' + width + '&childId=' + this.id + hash;
 
@@ -248,16 +248,31 @@
          */
         this._onHeightMessage = function(message) {
             /*
-             * Handle parent message from child.
+             * Handle parent height message from child.
              */
             var height = parseInt(message);
-            
+
             this.iframe.setAttribute('height', height + 'px');
         };
 
+        /**
+         * Scroll parent to a given id.
+         *
+         * @memberof Parent.prototype
+         * @method _onScrollToMessage
+         * @param {String} message The id of the element to scroll to.
+         */
+        this._onScrollToMessage = function(message) {
+            /*
+             * Handle parent scroll message from child.
+             */
+             document.location.href = '#' + message;
+        }
 
         /**
          * Bind a callback to a given messageType from the child.
+         *
+         * Reserved message names are: "height" and "scrollTo".
          *
          * @memberof Parent.prototype
          * @method onMessage
@@ -303,8 +318,9 @@
             this.settings[key] = config[key];
         }
 
-        // Add height event callback 
+        // Bind required message handlers
         this.onMessage('height', this._onHeightMessage);
+        this.onMessage('scrollTo', this._onScrollToMessage);
 
         // Add a listener for processing messages from the child.
         var that = this;
@@ -336,12 +352,14 @@
 
         this.messageRegex = null;
         this.messageHandlers = {};
-        
+
         // ensure a config object
         config = (config || {});
-        
+
         /**
          * Bind a callback to a given messageType from the child.
+         *
+         * Reserved message names are: "width".
          *
          * @memberof Child.prototype
          * @method onMessage
@@ -407,6 +425,33 @@
         };
 
         /**
+         * Resize iframe in response to new width message from parent.
+         *
+         * @memberof Child.prototype
+         * @method _onWidthMessage
+         * @param {String} message The new width.
+         */
+        this._onWidthMessage = function(message) {
+            /*
+             * Handle width message from the child.
+             */
+            var width = parseInt(message);
+
+            // Change the width if it's different.
+            if (width !== this.parentWidth) {
+                this.parentWidth = width;
+
+                // Call the callback function if it exists.
+                if (this.settings.renderCallback) {
+                    this.settings.renderCallback(width);
+                }
+
+                // Send the height back to the parent.
+                this.sendHeight();
+            }
+        };
+
+        /**
          * Send a message to the the Parent.
          *
          * @memberof Child.prototype
@@ -443,31 +488,15 @@
         };
 
         /**
-         * Resize iframe in response to new width message from parent.
+         * Scroll child to a given id.
          *
-         * @memberof Child.prototype
-         * @method _onWidthMessage
-         * @param {String} message The new width.
+         * @memberof Parent.prototype
+         * @method scrollTo
+         * @param {String} hash The id of the element to scroll to.
          */
-        this._onWidthMessage = function(message) {
-            /*
-             * Handle width message from the child.
-             */
-            var width = parseInt(message);
-
-            // Change the width if it's different.
-            if (width !== this.parentWidth) {
-                this.parentWidth = width;
-
-                // Call the callback function if it exists.
-                if (this.settings.renderCallback) {
-                    this.settings.renderCallback(width);
-                }
-
-                // Send the height back to the parent.
-                this.sendHeight();
-            }
-        };
+        this.scrollParentTo = function(hash) {
+            this.sendMessage('scrollTo', hash);
+        }
 
         // Identify what ID the parent knows this child as.
         this.id = _getParameterByName('childId') || config.id;
@@ -476,7 +505,7 @@
         // Get the initial width from a URL parameter.
         var width = parseInt(_getParameterByName('initialWidth'));
 
-        // Bind the width message handler
+        // Bind the required message handlers
         this.onMessage('width', this._onWidthMessage);
 
         // Initialize settings with overrides.
